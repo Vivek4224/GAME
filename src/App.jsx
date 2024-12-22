7cb1ad4c885bc3513181f9290cb2e2b02320ccc3
Vivek4224 <vivekbarvaliya4224@gmail.com>
2024-12-22 22:02:08 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import Features from "./Components/Features";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 };
